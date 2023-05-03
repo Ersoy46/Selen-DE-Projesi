d31ea9d385976b8e03d6be06663eb6af65500cba
Ertuğrul <tugrul121463@gmail.com>
2023-05-03 12:58:48 +0300

    first commit

---
+++ src/test/java/stepdefinitions/CommonStepDefinitions.java
@@ -0,0 +1,11 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class CommonStepDefinitions {
+
+
+
+}
---
+++ target/test-classes/features/01_navigations.feature
@@ -0,0 +1,5 @@
+Feature: navigasyon
+@google
+  Scenario: TC 01 navigasyon testi
+
+    Given kullanici https://www.google.com  adresine gider
